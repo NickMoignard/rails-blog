7a47027ff98b94bfed72dacd36570c32d6b5b333
nickmoignard <nicklmoignard@gmail.com>
2018-01-22 15:32:33 +1100

    added images to the posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+
+    #mount paperclip image
+    has_attached_file :image
+    validates_attachment :image,
+    content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
 end
