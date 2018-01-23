8f2a4b247b9006c39de81e3b8572fb8064e4c026
NickMoignard <nicklmoignard@gmail.com>
2018-01-23 12:59:28 +1100

    added a user relation to the posts model

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,5 @@ class Post < ApplicationRecord
     has_attached_file :image
     validates_attachment :image,
     content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
+    belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180122040535) do
+ActiveRecord::Schema.define(version: 20180123015106) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20180122040535) do
     t.string "image_content_type"
     t.integer "image_file_size"
     t.datetime "image_updated_at"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
