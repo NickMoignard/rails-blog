b2db647b1bb1c28fc37808d4db9762a4a683d493
nickmoignard <nicklmoignard@gmail.com>
2018-01-28 19:18:25 +1100

    integrated html5up template into the environment

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//
+//
+//
+//= require jquery.min
+//= require skel.min
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :posts
   devise_for :users
-  root 'posts#index'
+
+  get '/home', to: 'home#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
